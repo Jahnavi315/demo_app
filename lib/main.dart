030a0bf8c28a497bb7600993530daef999c53ace
Jahnavi315 <jahnavikanukuntla2003@gmail.com>
2024-01-05 22:04:45 +0530

    eodc 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
 import 'package:demo_app/views/screens/auth/login_screen.dart';
 import 'package:flutter/material.dart';
 
+var kcolorScheme = ColorScheme.fromSeed(
+  seedColor: Color.fromARGB(255, 177, 120, 51)
+);
+var kDarkColorScheme = ColorScheme.fromSeed(
+  brightness: Brightness.dark,// to say that this seed clr is used for dark mode
+  seedColor: const Color.fromARGB(255, 5, 99, 125)
+);
+
 void main() {
   runApp(const MyApp());
 }
@@ -8,29 +16,51 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      darkTheme: ThemeData.dark().copyWith(
         useMaterial3: true,
+        colorScheme: kDarkColorScheme,
+        cardTheme:const  CardTheme().copyWith(
+          color: kDarkColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(horizontal: 16,vertical: 8)
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kDarkColorScheme.primaryContainer,
+            foregroundColor: kDarkColorScheme.onPrimaryContainer
+          )
+        ),
+      ),
+      themeMode: ThemeMode.system,
+      theme: ThemeData().copyWith(
+        useMaterial3: true,
+        colorScheme: kcolorScheme,
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: kcolorScheme.onPrimaryContainer,
+          foregroundColor: kcolorScheme.primaryContainer,
+          centerTitle: false,
+          //titleTextStyle: TextStyle(fontSize: 25,color: kcolorScheme.primary),
+        ),
+        cardTheme:const  CardTheme().copyWith(
+          color: kcolorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(horizontal: 16,vertical: 8)
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kcolorScheme.primaryContainer
+          )
+        ),
+        textTheme: ThemeData().textTheme.copyWith(
+          titleLarge: TextStyle(
+            color: kcolorScheme.onSecondaryContainer,
+            fontSize: 16,
+            fontWeight: FontWeight.bold
+          )
+        )
       ),
       home: const LoginScreen(),
     );
--- lib/views/screens/auth/login_screen.dart
+++ lib/views/screens/auth/login_screen.dart
@@ -5,10 +5,73 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text(
-          'Login Screen'
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.primaryContainer,
+      body: Padding(
+        padding: const EdgeInsets.all(18.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Login Account',
+              style: TextStyle(
+                fontSize: 25,
+                color: Theme.of(context).colorScheme.primary,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 2
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            TextFormField(
+              decoration: const InputDecoration(
+                labelText: 'Email Addresss',
+              ),
+              keyboardType: TextInputType.emailAddress,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            TextFormField(
+              decoration: const InputDecoration(
+                labelText: 'Password'
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            // Container(
+            //   height: 50,
+            //   width: MediaQuery.of(context).size.width - 40,
+            //   color: Colors.amber,
+            // ),
+            TextButton(
+              onPressed: () {
+                
+              }, 
+              child: Text(
+                'Login',
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Theme.of(context).colorScheme.primary,
+                  fontWeight: FontWeight.bold
+                ),
+              )
+            ),
+            TextButton(
+              onPressed: () {
+                
+              }, 
+              child: Text(
+                'Need an account',
+                style: TextStyle(
+                  fontSize: 15,
+                  color: Theme.of(context).colorScheme.primary,
+                ),
+              )
+            )
+          ],
         ),
       ),
     );
